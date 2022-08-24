2e311570b495b06f8b8e14aadf9f88d33a2baed7
YigitAykol <61661801+YigitAykol@users.noreply.github.com>
2022-08-24 08:46:43 +0300

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
