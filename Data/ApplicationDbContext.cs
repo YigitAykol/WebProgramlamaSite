a8b54ad5197d1656e4d2b262edb19ed52d3fa894
YigitAykol <61661801+YigitAykol@users.noreply.github.com>
2022-08-24 08:48:56 +0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data;
+
+public class ApplicationDbContext:DbContext
+{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<WebApplication4.Models.Album>? Album { get; set; }
+        public DbSet<WebApplication4.Models.Category>? Category { get; set; }
+        public DbSet<WebApplication4.Models.Admin>? Admin { get; set; }
+        
+       
+        
+
+}
+
